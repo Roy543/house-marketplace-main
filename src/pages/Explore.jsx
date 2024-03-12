e7e7500aa0eebe61efc94fc50e5a91e0cc558ca0
Roy543 <sahilsas88@gmail.com>
2024-03-12 21:24:07 +0100

    first commit

---
+++ src/pages/Explore.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import saleCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
+import Slider from "../components/Slider";
+
+import React from "react";
+
+
+const Explore = () => {
+  return (
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+
+      <main>
+       <Slider />
+
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for rent</p>
+          </Link>
+          <Link to="/category/sale">
+            <img
+              src={saleCategoryImage}
+              alt="sale"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for sale</p>
+
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Explore;
